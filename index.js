42a5026345d722f02d4b4bd427c4e52f1a23af2a
Kane Mathis <admin@kanemathis.com>
2022-04-01 09:47:22 -0700

    commit tree a37f39ec3c2f301e640b364f266eb47db8a18e30

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  
+
+
+function findMatching(arr, string){
+    let match = arr.filter(elem => elem.toLowerCase() === string.toLowerCase())
+    return match;
+}
+function fuzzyMatch(arr, string){
+    let match = arr.filter(elem => elem.startsWith(string))
+    return match;
+}
+
+function matchName (arr, string){
+    let match = arr.filter(elem => elem.name === string);
+    return match;
+}
+
+//console.log(matchName(drivers, 'Bobby'));
+
+
+
+
+
